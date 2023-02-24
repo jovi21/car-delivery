917947b81ee6d130867c2693564cea3bf4afd58a
LGomez96 <lindagomez96@gmail.com>
2023-02-24 18:41:29 -0500

    feat(icons):agrego iconos en fichero svg

--- src/app/shared/components/icon/icon.component.ts
+++ src/app/shared/components/icon/icon.component.ts
@@ -6,7 +6,7 @@ import { Component, Input } from '@angular/core';
   templateUrl: './icon.component.html',
   styleUrls: ['./icon.component.scss'],
   standalone: true,
-  imports: [CommonModule]
+
 })
 export class IconComponent {
   @Input() name!: string;
